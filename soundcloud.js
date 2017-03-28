de2c398cef46deee7b454419a67f0f2b662f2adc
Joel Varughese <jtvarughese@gmail.com>
2017-03-28 18:00:20 -0400

    initial soundcloud api commit

---
+++ soundcloud.js
@@ -0,0 +1,60 @@
+// SoundCloud object with client id
+SC.initialize({
+  client_id: 'fd4e76fc67798bfa742089ed619084a6'
+});
+
+SC.resolve("https://soundcloud.com/atlanticrecords/the-show-goes-on").then(function(response){
+  console.log(response)
+});
+
+// defines the Jukebox object
+function Jukebox(){
+// the code for what happens when you create a Jukebox object
+// goes here
+  this.player = SC.stream("/tracks/39646304")
+}
+
+var jukebox = new Jukebox()
+
+
+SC.stream('track').then(function(player){
+  this.player.play();
+});
+
+// targets the play button from the page and
+// stores a reference to it in the playButton variable
+// this play button has global scope
+var playButton=document.querySelector("#play")
+var pauseButton=document.querySelector("#pause")
+
+// defines the Jukebox prototype object
+Jukebox.prototype.play = function(){
+  this.player.then(function(response){
+    player.play();
+  })
+}
+
+Jukebox.prototype.pause = function(){
+  this.player.then(function(response){
+    player.pause();
+  })
+}
+
+// adds an event listener for when you click the play button
+// preventDefault prevents anchor tag going to next page
+playButton.addEventListener("click", function(event){
+  event.preventDefault();
+  jukebox.play()
+  SC.get("/tracks/39646304").then(function(response){
+    document.querySelector("#songTitle").setAttribute("href", response.permalink_url);
+    document.querySelector("#artist").setAttribute("href", response.user.permalink_url);
+    document.getElementById("genre").innerHTML = "Genre: " + response.genre;
+    document.getElementById("coverArt").src = response.artwork_url;
+    document.getElementById("releaseDate").innerHTML = "Release Date: " + response.release_month + '/' + response.release_day + '/'+ response.release_year;
+  });
+})
+
+pauseButton.addEventListener("click", function(event){
+  event.preventDefault();
+  jukebox.pause()
+})
